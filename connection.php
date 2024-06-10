c22b84b543c084fcf8f555884352766cc059c362
Jbreezy42 <140350883+Jbreezy42@users.noreply.github.com>
2024-06-11 09:18:21 +1200

    Create connection.php
    copied from cpanel

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+     
+     include "credentials.php";
+     
+     //create connection to database
+     //mysqli requires address, username, password and database name
+     $connection = new mysqli('localhost', $user, $pw, $db);
+     
+     //create variable to store records from our database table
+     $Records = $connection->prepare("select * from scpdatabase");
+     //run sql query
+     $Records->execute();
+     //store result of query
+     $Result = $Records->get_result();
+
+?>
